64d46b44acaebb8354ea40cbe349d67ce85cb5a1
my-doyeooni <my-rlaehhhhhdus@gmail.com>
2023-01-09 17:33:49 +0900

    첫화면사진구현및타이틀작성

--- nextpage.py
+++ nextpage.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.filedialog 
+from PIL import ImageTk, Image
+from tkinter import *
 
 #윈도우 생성
 root=tk.Tk()
@@ -8,24 +10,37 @@ root=tk.Tk()
 root.title('NextPage')
 
 #창의 크기 설정
-root.geometry("1000x400+100+100")
+root.geometry("1000x1000")
 
+#첫화면으로 이동하는 함수
+def click():
+    root.destroy()
+    import screen
 
+#파일 불러오기 실행 함수
 def openfile():
     global filename
     filename=tk.filedialog.askopenfilename(initialdir = "C:/oosp/data",
         title = "open file", filetypes = (("text file","*.txt"),("all files","*.*")))
 
+#'사진선택' 문구 출력 (label 사용)
 lab1=tk.Label(root,text="사진선택",font=('Arial 13 bold'),bg='white',fg="black",width=11,height=1)
-lab1.grid(row=0,column=0,padx=5,pady=20)
+lab1.place(x=70,y=70)
 
-#이미지 추가
-file_img=tk.PhotoImage(open("dog.jpg"))
-#file_img= tkinter.PhotoImage(file="C:/Users/Do Yeon/oosp/oosp/icon.jpg")
-file_img=file_img.subsample(50,50)
+#이미지 추가(jpg로 사용할 때 오류 뜸 // png로 불러올것)
+file_img=tk.PhotoImage(file="C:/Users/Do Yeon/oosp/oosp/back_icon.png")
+file_icon=tk.PhotoImage(file="C:/Users/Do Yeon/oosp/oosp/file_icon.png")
 
+#첫화면으로 이동하는 버튼 구현
+btn3 = tk.Button(root, image=file_img, command=click, width=50, height=50)
+btn3.pack(expand=1, anchor=CENTER)
+btn3.place(x=1,y=1)
 
-btn3 = tkinter.Button(root, overrelief="solid",width = 50, height = 50, command=openfile,bg="white",image=file_img)
-btn3.grid(row=0, column=1, padx=5, pady=20)
+#'사진 선택' 불러오기 구현
+btn4 = tkinter.Button(root, overrelief="solid",width = 50, height = 50, command=openfile,bg="white",image=file_icon)
+btn4.place(x=200,y=50)
+
+#불러온 사진 추가
+
 
 root.mainloop()
--- screen.py
+++ screen.py
@@ -1,17 +1,33 @@
 from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
 
 root = Tk()
 
-root.geometry("1000x400+100+100")
+root.geometry("1000x1200")
 
 def click():
     root.destroy()
     import nextpage
 
+#제목 설정
+lab1 = ttk.Label(root, text="사진 필터 변환 프로그램",padding=(40,0), foreground="black", background="white", font=("Times", "20"))
+lab1.pack()
 
+#다음페이지로 넘겨주는 버튼 구현
 btn1 = Button(root, text ='시작하기')
 btn1.config(command=click)
 btn1.pack()
+
+
+#첫화면 사진 넣기 구현
+#file_img=tk.PhotoImage(file="C:/Users/Do Yeon/oosp/oosp/IMG_9096.png")
+img = PhotoImage(file="C:/Users/Do Yeon/oosp/oosp/image_picture.png")
+#img = img.resize((450,350), Image.ANTIALIAS)
+#main_img = ImageTk.PhotoImage(img)
+main_img = Label(image = img)
+main_img.pack(padx=30, pady=100)
+
 
 
 root.mainloop()
