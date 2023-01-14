11093c34f0a8792d46668daa029d69c500669e51
my-doyeooni <my-rlaehhhhhdus@gmail.com>
2023-01-14 22:44:28 +0900

    어두움 효과 구현

--- Darkness.py
+++ Darkness.py
@@ -18,24 +18,40 @@ img_path='C:/Users/Do Yeon/oosp/oosp'
 
 
 #어두움 조절 구현
-img = cv2.imread(img_path+'/'+"sea.png")
+img = cv2.imread(img_path+'/'+"sea.png", cv2.IMREAD_COLOR)
 
 def Darkness(original_image):
-    plt.figure(figsize=(5,5))
-    alpha = 1.0
+    val = 100
+    array = np.full(img.shape, (val, val,val), dtype=np.uint8)
+    sub = cv2.subtract(img, array) #어둡게 함
+
+    plt.figure(figsize=(5,5)) 
+    #alpha = 1.0
     #plt.figure(figsize=(50,50))
+
+    '''
     plt.subplot(121)
     plt.imshow(cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB))
     plt.title('Original')
     plt.xticks([]), plt.yticks([])
+    '''
+   
 
     #bright = cv2.add(whale, (15,15,15,0))
-    dark = np.clip((1+alpha) * img - 128 * alpha, 0, 255).astype(np.uint8)
+    #dark = np.ones(img.shape, dtype= "uint8") * 50
+    #sub = cv2.subtract(img, dark)
+    '''
     plt.subplot(122)
-    plt.imshow(dark)
+    plt.imshow(sub)
     plt.title('Darkness')
     plt.xticks([]), plt.yticks([])
     plt.show()
+    '''
 
+    numpy_horizontal = np.hstack((img, sub))
+
+    cv2.imshow('Darkness', numpy_horizontal)
 #plt.figure(figsize=(6,8))
+
 Darkness(img)
+cv2.waitKey()
