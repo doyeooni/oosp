fea3fee3d280fba6d92df7b6d72a84436989208f
my-doyeooni <my-rlaehhhhhdus@gmail.com>
2023-01-17 03:25:16 +0900

    코드 변경

--- Brightness.py
+++ Brightness.py
@@ -17,8 +17,27 @@ import numpy as np
 
 img_path='C:/Users/Do Yeon/oosp/oosp'
 
+def open_file():
+    global my_image
+    global my_path
+    global path
+    filename = filedialog.askopenfilename(initialdir='', title='파일선택', filetypes=(
+    ('png files', '*.png'), ('jpg files', '*.jpg'), ('all files', '*.*')))
+ 
+    #Label(root, text=root.filename).pack() # 파일경로 view
+    my_path = Label(text=filename)
+    my_path.place(x=700,y=1)
+    path = str(my_path)
+
+    my_image = PhotoImage(file=filename)
+    my_img = Label(image=my_image)
+    my_img.pack()
+    my_img.place(x=100, y=50)
+
+
+    
 #밝기 조절 구현
-img = cv2.imread(img_path+'/'+"sea.png", cv2.IMREAD_COLOR)
+img = cv2.imread(img_path+'/'+"dog.png", cv2.IMREAD_COLOR)
 
 def Bright(original_image):
     val = 50
